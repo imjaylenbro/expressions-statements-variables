0c450dc8f61812202188ae359046f52f58bff9c9
imjaylenbro <55603069+imjaylenbro@users.noreply.github.com>
2020-07-14 23:04:22 -0700

    adding some consloe log

--- global.js
+++ global.js
@@ -7,27 +7,29 @@ console.log(4 + 5);
 
 //1.) Log an expression that evaluates to the sum of two integers
 // your code here
-console.log("answer 1");
+console.log(3 + 5);
 
 // 2.) log an expression that evaluates to the difference of two integers
 // your code here
-console.log("answer 2");
+console.log(8 - 6);
 
 // 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
 // your code here
-console.log("answer 3");
+console.log("I'm writing" + "my first JavaScript code");
 
-// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than each other
 // your code here
-console.log("answer 4");
+console.log(5 > 10);
 
 // 5.) Log an expression that uses modulo, with a result of 4
 // your code here
-console.log("answer 5");
+console.log(13 / 3);
 
 // 6.) Create two different strings and compare them, logging the result.
 // It should be false!
-console.log("answer 6");
+let x = "10";
+let y = "40";
+console.log("x" > "y");
 
 // 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
 console.log("answer 7");
